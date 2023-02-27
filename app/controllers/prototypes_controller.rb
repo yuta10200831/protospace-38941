77e7bf4c5b92b4c9fc4f50ccb38d4d6af2a286b5
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-02-27 17:02:58 +0900

    ユーザー名の表示、ページにとどまる

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -14,7 +14,7 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to root_path(@prototype)
     else
-      render :index
+      render :Prototypes/new
     end
   end
 
