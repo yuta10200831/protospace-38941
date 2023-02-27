bee612922ea56bd5c67c24fd58dc33405bd6c7b9
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-02-27 19:04:28 +0900

    とどまる修正⑦　action変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-      render action: :prototypes/show
+      render template: "prototypes/show"
     end
   end
 
